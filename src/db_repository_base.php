15187d25b86540bf5fe344016cbbdbe1bde33da8
thernadi <106915669+thernadi@users.noreply.github.com>
2023-07-18 08:20:30 +0200

    transaction mod

--- src/db_repository_base.php
+++ src/db_repository_base.php
@@ -370,6 +370,53 @@ trait DbRepositoryBase
 		}
 	}
 
+	public function saveWithTransaction($item) 
+	{
+		try
+		{
+			$this->beginTransaction();
+			$this->save($item);
+			$this->commitTransaction();
+		}
+		catch (\Throwable $e)
+		{
+			$this->rollbackTransaction();
+			throw $e;
+		}
+	}
+
+	public function loadByIdWithTransaction($id)
+	{
+		$returnValue = null;
+		try
+		{
+			$this->beginTransaction();
+			$returnValue = $this->loadById($id);
+			$this->commitTransaction();
+		}
+		catch (\Throwable $e)
+		{
+			$this->rollbackTransaction();
+			throw $e;
+		}		
+		return $returnValue;
+	}
+
+	public function deleteWithTransaction($item)
+	{
+		try
+		{
+			$this->beginTransaction();
+			$this->delete($item);
+			$this->commitTransaction();
+		}
+		catch (\Throwable $e)
+		{
+			$this->rollbackTransaction();
+			throw $e;
+		}
+	}
+
 	//$name: attribute name
 	//$value: search value	
 	//$items: ItemAttribute array
